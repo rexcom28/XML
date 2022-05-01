da769cbb53ca415306b5807a8ec7f1021fb7596b
Francisco Rodriguez <franciscojavierrod28@gmail.com>
2022-05-01 10:57:33 -0600

    3rt Commit

--- Reader_XML/settings.py
+++ Reader_XML/settings.py
@@ -1,4 +1,4 @@
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -177,3 +177,5 @@ else:
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
